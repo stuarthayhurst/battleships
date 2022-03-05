3af68084b17c96556830b41491f4886af5670577
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-03-05 13:47:42 +0000

    Moved game into a class

--- battleships.py
+++ battleships.py
@@ -11,106 +11,125 @@ pieceInfo = {
   "p": ["patrol boat", 2]
 }
 
-#Return a list of blank grids (1 for each controller)
-def createGrids(gridWidth, gridHeight):
-  if gridWidth <= 26 and gridHeight <= 26:
-    return [[["0" for x in range(gridWidth)] for i in range(gridHeight)] for grid in range (2)]
-  else:
-    return False
+class GameController:
+  def __init__(self, pieceIdentifiers, pieceInfo):
+    self.pieceIdentifiers = pieceIdentifiers
+    self.pieceInfo = pieceInfo
+    self.controllers = [0, 0]
 
-#Print the passed grid, hiding any ships if specified
-def drawGrid(grid, hideShips):
-  #Print alphabetical grid references as header
-  print("   ", end = "")
+  #Return a list of blank grids (1 for each controller)
+  def createGrids(self, gridWidth, gridHeight):
+    if gridWidth <= 26 and gridHeight <= 26:
+      return [[["0" for x in range(gridWidth)] for i in range(gridHeight)] for grid in range (2)]
+    else:
+      return False
 
-  for i in range(len(grid[0])):
-    #Print each alphabetical grid reference
-    print(chr(i + 65), end = "")
-  print()
+  #Print the passed grid, hiding any ships if specified
+  def drawGrid(self, grid, hideShips):
+    #Print alphabetical grid references as header
+    print("   ", end = "")
 
-  #Iterate over grid coords
-  for rowNum, row in enumerate(grid):
-    #Prefix a zero if single digit
-    rowNum += 1
-    if rowNum < 10:
-      print("0", end = "")
-    print(f"{rowNum} ", end = "")
-
-    for col in row:
-      #If the piece isn't a ship, or ships are being shown, show the actual piece
-      if col not in pieceIdentifiers or hideShips == False:
-        print(col, end = "")
-      #Otherwise, censor any ships
-      else:
-        print("0", end = "")
+    for i in range(len(grid[0])):
+      #Print each alphabetical grid reference
+      print(chr(i + 65), end = "")
     print()
 
-def checkWinner(grid):
-  #Return true if all pieces are gone
-  for row in grid:
-    for i in row:
-      if i != "0":
-        return False
-  return True
+    #Iterate over grid coords
+    for rowNum, row in enumerate(grid):
+      #Prefix a zero if single digit
+      rowNum += 1
+      if rowNum < 10:
+        print("0", end = "")
+      print(f"{rowNum} ", end = "")
 
-def placeMove(grid, usedMoves, move):
-  hitTile = grid[move[1]][move[0]]
+      for col in row:
+        #If the piece isn't a ship, or ships are being shown, show the actual piece
+        if col not in self.pieceIdentifiers or hideShips == False:
+          print(col, end = "")
+        #Otherwise, censor any ships
+        else:
+          print("0", end = "")
+      print()
 
-  #If guess was a miss, blank that tile and return
-  if hitTile not in pieceIdentifiers:
-    usedMoves[move[1]][move[0]] = " "
-    print("Miss!")
-    return
+  def checkWinner(self, grid):
+    #Return true if all pieces are gone
+    for row in grid:
+      for i in row:
+        if i != "0":
+          return False
+    return True
 
-  print("Hit!")
-  usedMoves[move[1]][move[0]] = "X"
-  grid[move[1]][move[0]] = "0"
+  def placeMove(self, grid, usedMoves, move):
+    hitTile = grid[move[1]][move[0]]
 
-  #Return if the full ship hasn't been hit
-  for row in grid:
-    if hitTile in row:
+    #If guess was a miss, blank that tile and return
+    if hitTile not in self.pieceIdentifiers:
+      usedMoves[move[1]][move[0]] = " "
+      print("Miss!")
       return
 
-  #Whichever ship was hit is no longer on the grid, announce it sank
-  print(f"Enemy {pieceInfo[hitTile][0]} was sunk!")
+    print("Hit!")
+    usedMoves[move[1]][move[0]] = "X"
+    grid[move[1]][move[0]] = "0"
 
-#Initialise grids (standard 7x7)
-grids = createGrids(7, 7) #Store player ships
-moves = createGrids(7, 7) #Store moves made
-if not grids:
+    #Return if the full ship hasn't been hit
+    for row in grid:
+      if hitTile in row:
+        return
+
+    #Whichever ship was hit is no longer on the grid, announce it sank
+    print(f"Enemy {self.pieceInfo[hitTile][0]} was sunk!")
+
+  def setup(self, gridWidth, gridHeight):
+    #Initialise grids
+    self.grids = self.createGrids(gridWidth, gridHeight) #Store player ships
+    self.moves = self.createGrids(gridWidth, gridHeight) #Store moves made
+
+    if not self.grids:
+      return False
+    return True
+
+  def addPlayers(self, controllers):
+    #Create a controller for each player
+    self.controllers[0] = controllers[0](self.pieceIdentifiers, self.pieceInfo, self.drawGrid, 1)
+    self.controllers[1] = controllers[1](self.pieceIdentifiers, self.pieceInfo, self.drawGrid, 2)
+
+  def start(self):
+    os.system("cls||clear")
+    self.controllers[0].placeShips(self.grids[0])
+    os.system("cls||clear")
+    self.controllers[1].placeShips(self.grids[1])
+
+    while True:
+      #Get next move from controller, using existing moves
+      move = self.controllers[0].nextMove(self.moves[0])
+      #Update enemy grid and made moves grids
+      self.placeMove(self.grids[1], self.moves[0], move)
+      #If the game is over, exit
+      if self.checkWinner(self.grids[1]):
+        winner = "Player 1"
+        break
+
+      #Wait for next player
+      input("\nPress any key to continue")
+
+      #Same as controller 1
+      move = self.controllers[1].nextMove(self.moves[1])
+      self.placeMove(self.grids[0], self.moves[1], move)
+      if self.checkWinner(self.grids[0]):
+        winner = "Player 2"
+        break
+
+      input("\nPress any key to continue")
+
+    print(f"{winner} wins!")
+
+game = GameController(pieceIdentifiers, pieceInfo)
+
+#Use standard 7x7 setup
+if not game.setup(7, 7):
   input("Failed to create grids, exiting")
   exit(1)
 
-#Create controllers, and pass drawGrid method
-controllers = [0, 0]
-controllers[0] = player.Player(pieceIdentifiers, pieceInfo, drawGrid, 1)
-controllers[1] = player.Player(pieceIdentifiers, pieceInfo, drawGrid, 2)
-
-controllers[0].placeShips(grids[0])
-os.system("cls||clear")
-controllers[1].placeShips(grids[1])
-
-while True:
-  #Get next move from controller, using existing moves
-  move = controllers[0].nextMove(moves[0])
-  #Update enemy grid and made moves grids
-  placeMove(grids[1], moves[0], move)
-  #If the game is over, exit
-  if checkWinner(grids[1]):
-    winner = "Player 1"
-    break
-
-  #Wait for next player
-  input("\nPress any key to continue")
-
-  #Same as controller 1
-  move = controllers[1].nextMove(moves[1])
-  placeMove(grids[0], moves[1], move)
-  if checkWinner(grids[0]):
-    winner = "Player 2"
-    break
-
-  input("\nPress any key to continue")
-
-
-print(f"{winner} wins!")
+game.addPlayers([player.Player, player.Player])
+game.start()
