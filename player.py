7ea7d69562f695ecb89e96266cd78c108e520c19
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-03-05 15:37:58 +0000

    Moved some methods out to grid helpers, to be shared with AI implementation in future

--- player.py
+++ player.py
@@ -8,86 +8,13 @@ class Player:
     self.drawGrid = drawGrid
     self.playerNum = playerNum
 
+  def passHelpers(self, gridHelpers):
+    self.gridHelpers = gridHelpers
+
   #Helper function to clear the screen and display player number
   def resetScreen(self):
     os.system("cls||clear")
     print(f"Player {self.playerNum}:\n")
-
-  #Helper function to convert user input into valid coords
-  def inputToReference(self, position, grid):
-    #Split position into a separate row and column value, attempt to guess format
-    if "," in position:
-      position = position.split(",")
-    else:
-      position = position.split(" ")
-    if len(position) != 2:
-      input("Grid reference must be in the format 'col, row'")
-      return None, None
-
-    #Convert to expected data types
-    x = str(position[0])
-    try:
-      y = int(position[1])
-    except ValueError:
-      input("Row must be an integer")
-      return None, None
-
-    #Convert input to grid position
-    try:
-      x = ord(x) - 97
-    except TypeError:
-      input("Alphabetical grid reference must be a single character")
-      return None, None
-    y -= 1
-
-    #Check coords fall inside grid boundaries
-    if y > len(grid) - 1 or x > len(grid[0]) - 1 or y < 0 or x < 0:
-      input("Coordinates must be within grid boundaries")
-      return None, None
-
-    #If the input was valid, return coords
-    return x, y
-
-  def placePiece(self, grid, piece, flipped, position):
-    x, y = self.inputToReference(position, grid)
-
-    #Check coords were actually returned
-    if x == None:
-      return False
-
-    #Return false if the ship won't fit
-    shipLength = self.pieceInfo[piece][1]
-    pieceName = self.pieceInfo[piece][0].capitalize()
-    if not flipped:
-      if x + shipLength > len(grid[0]):
-        input(f"{pieceName} won't fit there")
-        return False
-    else:
-      if y + shipLength > len(grid):
-        input(f"{pieceName} won't fit there")
-        return False
-
-    #Return false if another ship is in the way
-    if not flipped:
-      for i in range(x, x + shipLength):
-        if grid[y][i] != "0":
-          input(f"{pieceName} would collide with another ship")
-          return False
-    else:
-      for i in range(y, y + shipLength):
-        if grid[i][x] != "0":
-          input(f"{pieceName} would collide with another ship")
-          return False
-
-    #Place the ship on the grid
-    if not flipped:
-      for i in range(x, x + shipLength):
-        grid[y][i] = piece
-    else:
-      for i in range(y, y + shipLength):
-        grid[i][x] = piece
-
-    return True
 
   def placeShips(self, grid):
     #Wait for input, to allow any players to swap
@@ -116,7 +43,7 @@ class Player:
         if action == "f":
           flipped = not flipped
         else:
-          if self.placePiece(grid, piece, flipped, action):
+          if self.gridHelpers.placePiece(grid, piece, flipped, action, True):
             placing = False
 
     self.resetScreen()
@@ -131,7 +58,7 @@ class Player:
 
       #Take input and validate position
       position = input("Enter a grid reference to fire at (x, y): ")
-      x, y = self.inputToReference(position, usedMoves)
+      x, y = self.gridHelpers.inputToReference(position, usedMoves, True)
 
       #Check coords were actually returned
       if x == None:
