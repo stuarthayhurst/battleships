432c9e3ae68dbec14052f2f32c7d6ec53f29c199
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2023-01-22 17:40:35 +0000

    Added random opponent

---
+++ opponents/random.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import random
+
+class Opponent:
+  def __init__(self):
+    self.opponentGrid = [[0 for i in range(7)] for j in range(7)]
+
+  def makeMove(self):
+    valid = False
+    while not valid:
+      x, y = random.randint(0, 6), random.randint(0, 6)
+
+      valid = self.opponentGrid[y][x] == 0
+
+    self.opponentGrid[y][x] = 1
+    return [x, y]
+
+  def feedbackMove(self, wasHit, didSink):
+    pass
