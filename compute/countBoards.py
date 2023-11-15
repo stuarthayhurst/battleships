07eb0c4ec0cc7c6f91fb9a75871f6f2f715e3860
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2023-11-15 13:28:58 +0000

    Output performance metrics in Python implementation

--- compute/countBoards.py
+++ compute/countBoards.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import time
+
 def placePiece(grid, length, x, y, flipped):
   #Return false if another ship is in the way
   if not flipped:
@@ -54,6 +56,10 @@ boardSize = 7
 n = 0
 initialValidShips = [i for i in range(len(shipData))]
 grid = [[0 for i in range(boardSize)] for j in range(boardSize)]
-compute(initialValidShips, grid)
 
-print(f"{n} valid combinations")
+startTime = time.time()
+compute(initialValidShips, grid)
+deltaTime = time.time() - startTime
+
+print(f"\nFound {n} boards in {round(deltaTime, 2)} seconds")
+print(f"{round((n / deltaTime), 2)} boards / second");
